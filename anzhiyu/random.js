cb2996db44e1efe697e00e270d2b148f7ab666fb
Likai2002 <642701893@qq.com>
2026-06-09 21:23:29 +0800

    Site updated: 2026-06-09 21:23:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/02/hello-world/","2026/06/09/博客上线啦！博客诞生的心路历程/"];function toRandomPost(){
+var posts=["2026/06/09/content/","2026/06/02/hello-world/","2026/06/09/博客上线啦！博客诞生的心路历程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
